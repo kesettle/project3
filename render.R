a2bcfe9e6fe0e9e0b54d4ca8b18a69b7d52f318d
kesettle <kesettle@ncsu.edu>
2022-10-13 14:21:48 -0400

    initial commit

---
+++ render.R
@@ -0,0 +1,10 @@
+#create README.md to render vignette
+rmarkdown::render(
+  input="project3.Rmd",
+  output_format = "github_document",
+  output_file = "README.md"
+  output_options = list(
+    df_print = "default",
+    html_preview = FALSE # to remove .html file creation
+  )
+)
