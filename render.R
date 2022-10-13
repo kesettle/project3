1830aef98ea3233338193bbdcbd411094157c396
kesettle <kesettle@ncsu.edu>
2022-10-13 14:22:28 -0400

    initial commit

--- render.R
+++ render.R
@@ -2,7 +2,7 @@
 rmarkdown::render(
   input="project3.Rmd",
   output_format = "github_document",
-  output_file = "README.md"
+  output_file = "README.md",
   output_options = list(
     df_print = "default",
     html_preview = FALSE # to remove .html file creation
